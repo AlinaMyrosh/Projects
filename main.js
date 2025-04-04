e8bb3de5e243b085fddcd7d4e1cd70871d9146b9
AlinaMyrosh <154742742+AlinaMyrosh@users.noreply.github.com>
2025-04-04 21:52:14 +0200

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+// const swiper = new Swiper('.swiper', {
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     allowSlideNext: true,
+//     allowSlidePrev: true,
+//     allowTouchMove: true,
+//     effect: 'slide',
+//     mousewheel: {
+//         invert: true,
+//     },
+//     slideNextClass: 'projects-prev-btn',
+//     slidePrevClass: 'projects-next-btn',
+//     spaceBetween: 24,
+// });
